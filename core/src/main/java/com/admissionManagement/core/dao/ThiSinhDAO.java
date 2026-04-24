fcb6cdccb748828d38bd7c223c402e49a00160b2
Nguyễn Đức thành <nguyenducthanh029@gmail.com>
2026-04-24 21:15:31 +0700

    4 file cuoi

--- core/src/main/java/com/admissionManagement/core/dao/ThiSinhDAO.java
+++ core/src/main/java/com/admissionManagement/core/dao/ThiSinhDAO.java
@@ -1,5 +1,6 @@
 package com.admissionManagement.core.dao;
 
+import com.admissionManagement.core.entity.BangQuyDoi;
 import com.admissionManagement.core.entity.ThiSinh;
 import com.admissionManagement.core.util.HibernateUtil;
 import org.hibernate.Session;
@@ -17,71 +18,25 @@ public class ThiSinhDAO {
         this.factory = HibernateUtil.getSessionFactory();
     }
 
-    public void addThiSinh(ThiSinh ts) {
-        Session session = factory.openSession();
-        Transaction tx = null;
-        try {
-            tx = session.beginTransaction();
-            session.save(ts);
-            tx.commit();
-        } catch (Exception e) {
-            if(tx != null) tx.rollback();
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
+    public void addWithSession(Session session, ThiSinh thiSinh) {
+        session.save(thiSinh);
     }
 
-    public List<ThiSinh> getAllThiSinh(){
-        Session session = factory.openSession();
+    public ThiSinh getWithSession(Session session, int id) {
+        ThiSinh thiSinh = session.get(ThiSinh.class, id);
+        return thiSinh;
+    }
+    public List<ThiSinh> getAllWithSession(Session session){
         String query = "FROM ThiSinh";
         List listThiSinh = session.createQuery(query).list();
-        session.close();
         return listThiSinh;
     }
 
-    public void updateThiSinh(int id, ThiSinh newThiSinh) {
-        Session session = factory.openSession();
-        Transaction tx = null;
-        ThiSinh thiSinh = session.get(ThiSinh.class, id);
-        try {
-            tx = session.beginTransaction();
-            if(thiSinh != null){
-                thiSinh.setGioiTinh(newThiSinh.getGioiTinh());
-                thiSinh.setCccd(newThiSinh.getCccd());
-                thiSinh.setSoBaoDanh(newThiSinh.getSoBaoDanh());
-                thiSinh.setHo(newThiSinh.getHo());
-                thiSinh.setTen(newThiSinh.getTen());
-                thiSinh.setDienThoai(newThiSinh.getDienThoai());
-                thiSinh.setEmail(newThiSinh.getEmail());
-                thiSinh.setPassword(newThiSinh.getPassword());
-                thiSinh.setNgaySinh(newThiSinh.getNgaySinh());
-                thiSinh.setNoiSinh(newThiSinh.getNoiSinh());
-                thiSinh.setDoiTuong(newThiSinh.getDoiTuong());
-                thiSinh.setKhuVuc(newThiSinh.getKhuVuc());
-                thiSinh.setUpdatedAt(LocalDate.now());
-            }
-            tx.commit();
-        } catch (Exception e) {
-            if(tx != null) tx.rollback();
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
+    public void updateWithSession(Session session, ThiSinh newThiSinh) {
+        session.merge(newThiSinh);
     }
 
-    public void deleteThiSinh(int id) {
-        Session session = factory.openSession();
-        Transaction tx = null;
-        try {
-            tx = session.beginTransaction();
-            ThiSinh ts = session.get(ThiSinh.class,id);
-            if (ts != null) session.delete(ts);
-            tx.commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
+    public void deleteWithSession(Session session, ThiSinh thiSinh) {
+        session.detach(thiSinh);
     }
 }
